7abfb5f08e382d4d0150847d4667dcf6e797c85b
Dragon <1239209219@qq.com>
2022-10-09 16:32:49 +0800

    Third Commit

--- src/main/java/com.Dragon.git/Git.java
+++ src/main/java/com.Dragon.git/Git.java
@@ -4,7 +4,8 @@ public class Git {
 
        public static void main(String[] args) {
         System.out.println("hello Git");
-           System.out.println("hello GitHub");
+        System.out.println("hello GitHub");
+        System.out.println("hello Dragon");
     }
 
 }
